42a9bc40cd03a5adbf6df5223587fe598be438d4
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2024-12-13 11:20:46 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -387,7 +387,7 @@ const Home = () => {
 
       {/* Chatbot Button */}
       <a
-        href="https://mineoai.streamlit.app/"
+        href="https://forknife.streamlit.app/"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-600 text-white p-4 rounded-full shadow-lg hover:bg-green-700 transition-all w-16 h-16 flex items-center justify-center"
